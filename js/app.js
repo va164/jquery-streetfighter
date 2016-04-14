88bf331a08116340924cb0f723ab18fab1f5504c
va164 <victor.ahluwalia@gmail.com>
2016-04-14 13:53:48 -0400

    added animations related to mouse events

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
+	$('.conor').mouseenter(function() {
+			console.log('mouse entered .conor div');
+			$(".conor-still").hide();
+			$(".conor-ready").show();
+	})
+	.mouseleave(function() {
+		console.log("mouse left");
+		$(".conor-still").show();
+		$(".conor-ready").hide();
+		})
+	.mousedown(function() {
+		console.log("mouse clicked");
+		$(".conor-ready").hide();
+		$(".conor-strike").show();
+		$(".fire").show();
 
+		})
+	.mouseup(function() {
+		console.log("mouse un-clicked");
+		$(".conor-strike").hide();
+		$(".conor-ready").show();
+
+		});
 });
