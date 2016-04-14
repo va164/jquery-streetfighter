8a642504242a920e71837f3de60ddb32342bc851
va164 <victor.ahluwalia@gmail.com>
2016-04-14 14:22:41 -0400

    animated fire

--- js/app.js
+++ js/app.js
@@ -10,16 +10,24 @@ $(document).ready(function() {
 		$(".conor-ready").hide();
 		})
 	.mousedown(function() {
+		// play fire sound 
 		console.log("mouse clicked");
 		$(".conor-ready").hide();
 		$(".conor-strike").show();
-		$(".fire").show();
-
+		$(".fire").finish().show()
+		.animate(
+			{"left": "1100px"},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '600px')
+			});	
 		})
 	.mouseup(function() {
 		console.log("mouse un-clicked");
 		$(".conor-strike").hide();
 		$(".conor-ready").show();
-
 		});
-});
+
+
+	});
