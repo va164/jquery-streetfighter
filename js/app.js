3d150b0f4a5f5b9f006e6a123403f0186d7bc4b2
va164 <victor.ahluwalia@gmail.com>
2016-04-16 16:15:25 -0400

    added taunt functions

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
+
 $(document).ready(function() {
+
 	$('.conor').mouseenter(function() {
 			console.log('mouse entered .conor div');
 			$(".conor-still").hide();
@@ -8,6 +10,7 @@ $(document).ready(function() {
 		console.log("mouse left");
 		$(".conor-still").show();
 		$(".conor-ready").hide();
+		
 		})
 	.mousedown(function() {
 		playFire(); 
@@ -30,9 +33,23 @@ $(document).ready(function() {
 		});
 
 	});
-	
+
 function playFire() {
 	$("#fire-sound")[0].volume = .5;
 	$("#fire-sound")[0].load();
 	$("#fire-sound")[0].play();
-}
+};
+
+$(document).keydown(function(event){
+	if (event.keycode = 88) {
+			$(".conor-ready").hide();
+			$(".conor-still").hide();
+			$(".conor-taunt").show();
+	}
+})
+	.keyup(function(event) {
+		if (event.keycode = 88) {
+			$(".conor-taunt").hide();
+			$(".conor-still").show();
+		}
+	});
